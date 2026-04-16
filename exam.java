c0d1331154a45f40878ad239e4beffb610c913f8
노현승 <seunglee0807@gmail.com>
2026-04-16 11:21:54 +0900

    제출

--- exam.java
+++ exam.java
@@ -1,20 +1,36 @@
 package class_diagram_exam;
 
-class Character {
-    Posion myPosion = new Posion();
-    Weapon myWeapon;
-
-    Character() {
+class Person {
+    String name;
+  }
+  
+  interface Borrowable {
+    void borrow();
+  }
+  
+  class BorrowRecord { }
+  
+  class Book {
+    void open() {
+      System.out.println("책을 펼칩니다!");
     }
-
-    public void getMyWeapon(Weapon w) {
+  }
+  
+  class Member extends Person implements Borrowable {
+    private BorrowRecord record;
+  
+    public Member() {
+        this.record = new BorrowRecord();
     }
-}
-class Posion {
-}
-
-class Weapon {
-}
-
-class Monster {
-}
+  
+    public void borrow()
+    {
+      System.out.println("대출 규칙에 따라 책을 빌립니다!");
+    }
+  
+    public void read(Book book) {
+      System.out.println("책을 읽습니다!");
+      book.open();
+    }
+  }
+  
